422394655a9f593ae47442cfcc0786749b56eced
Aaron Perdew <aperdew@staff.dospace.org>
2017-08-20 11:04:06 -0500

    setting up models

---
+++ MonopolyJS/Models/PlayerModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MonopolyJS.Models
+{
+    public class PlayerModel
+    {
+        public List<PropertyModel> propertiesHeld { get; set; }
+        public int money { get; set; }
+        public int turnOrder { get; set; }
+        public bool isHuman { get; set; }
+    }
+}
---
+++ MonopolyJS/Models/PropertyModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MonopolyJS.Models
+{
+    public class PropertyModel
+    {
+        public int value { get; set; }
+        public string name { get; set; }
+        public int numOfHouses { get; set; }
+        public int numOfHotels { get; set; }
+        public int costPerHouse { get; set; }
+        public int costPerHotel { get; set; }
+        public string owner { get; set; }
+        //to do: add color, picture, and other info
+    }
+}
